6a4b2bd679f97e68c85c288eff9e30719991f12a
meanmonkey662 <coolkoch8535@gmail.com>
2016-05-27 12:47:08 -0600

    created and added

---
+++ src/controller/QRunner.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class QRunner 
+{
+	public static void main(String [] args)
+	{
+		new QController();
+	}
+}
---
+++ src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer 
+{
+	private boolean hasOrder;
+	private String name;
+	private double total;
+	
+	public Customer()
+	{
+		hasOrder = true;
+		name = "Doom Doomery";
+		total = 100;
+	}
+}
